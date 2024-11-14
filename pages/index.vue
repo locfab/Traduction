7a7556ad0e27c98fc5b65c98c9dcbbc73198daea
locfab <locqueville.fabrice@gmail.com>
2024-11-14 20:27:49 +0100

    plus de mot fusion de fichier

--- pages/index.vue
+++ pages/index.vue
@@ -37,7 +37,7 @@ import CheckBox from "~/components/input/CheckBox.vue";
 
 const words = ref<Line[]>([])
 const currentLanguage = ref<string>('fr')
-const files = ref<string[]>(["verbes_A-E.csv", "interrogation.csv", "verbes_F-I.csv", "mots1.csv", "mots2.csv"])
+const files = ref<string[]>(["verbes_A-E.csv", "verbes_F-I.csv", "verbes_F-O.csv", "mots1.csv", "mots2.csv", "mots3.csv",  "interrogation.csv"])
 const selectedFiles = ref<string[]>([]) // Spread operator to copy the array
 
 const isRandom = ref<boolean>(true)
