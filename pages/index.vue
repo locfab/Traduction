8123e4e5ee930b02eb3e75fcc16895024cc2de28
locfab <locqueville.fabrice@gmail.com>
2024-11-19 21:08:47 +0100

    clean petit

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="link-to-add">
+  <div class="link-to-add" v-if="false">
     <NuxtLink to="/add">Ajouter un enregistrement</NuxtLink>
   </div>
   <div class="categories-container">
@@ -8,7 +8,7 @@
         :class="['category', isSelectedFile(file) ? 'selected' : '']"
         :key="file"
         @click="selectFile(file)">
-      <span>{{ file }} </span>
+      <span>{{ formatFileName(file) }} </span>
     </div>
   </div>
   <div class="options">
@@ -58,7 +58,6 @@
   </div>
 </template>
 
-
 <script setup lang="ts">
 import { ref, computed, onMounted } from 'vue';
 import LanguageSelector from "../components/LanguageSelector.vue";
@@ -67,49 +66,47 @@ import CheckBox from "~/components/input/CheckBox.vue";
 
 const words = ref<Line[]>([]);
 const currentLanguage = ref<string>('fr');
-const files = ref<string[]>(["verbes_A-I.csv", "verbes_J-O.csv", "verbes_P-R.csv", "verbes_S-T.csv", "mots1.csv", "mots2.csv", "mots3.csv", "mots4.csv", "interrogation.csv", "expressions.csv", "jours-mois.csv"]);
-const selectedFiles = ref<string[]>([]); // Spread operator to copy the array
+const files = ref<string[]>(["verb_A-I.csv", "verb_J-O.csv", "verb_P-R.csv", "verb_S-T.csv", "mots1.csv", "mots2.csv", "mots3.csv", "mots4.csv", "interrog.csv", "expr.csv", "expr2.csv", "jours-mois.csv"]);
+const selectedFiles = ref<string[]>([]);
 const isRandom = ref<boolean>(true);
 const showIndex = ref<boolean>(false);
-const search = ref<string>(''); // Variable pour la recherche
-const visibleAnswers = ref<boolean>(false); // Variable pour la recherche
+const search = ref<string>('');
+const visibleAnswers = ref<boolean>(false);
 
-// Fonction pour sélectionner un fichier
 function selectFile(file: string) {
   selectedFiles.value = isSelectedFile(file) ? selectedFiles.value.filter(sf => sf !== file) : [...selectedFiles.value, file];
   loadCSV();
 }
 
-// Fonction pour changer la langue
 function updateLanguage(language: string) {
   currentLanguage.value = language;
 }
 
-// Fonction pour vérifier si un fichier est sélectionné
 function isSelectedFile(file: string): boolean {
   return selectedFiles.value.includes(file);
 }
 
-// Interface de structure de ligne
+function formatFileName(file: string): string {
+  return file.replace('.csv', '');
+}
+
 interface Line {
   fr: string;
   phonetic: string;
   ar: string;
 }
 
-// Charger les fichiers CSV
 async function loadCSV() {
-  const tempWords = []; // Accumule les mots temporairement
-
+  const tempWords = [];
   const filesToLoad = selectedFiles.value.length ? selectedFiles.value : files.value;
 
   for (const file of filesToLoad) {
     const response = await fetch(file);
     const data = await response.text();
-    tempWords.push(...parseCSV(data)); // Ajoute les nouveaux mots dans le tableau temporaire
+    tempWords.push(...parseCSV(data));
   }
 
-  words.value = tempWords; // Mise à jour unique
+  words.value = tempWords;
 }
 
 function parseCSV(data: any) : Line[] {
@@ -132,25 +129,22 @@ const filteredWords = computed(() => {
   });
 });
 
-// Charger les fichiers au montage du composant
 onMounted(() => {
   loadCSV();
 });
-
 </script>
 
 <style>
-/* Conteneur principal */
 .container {
   text-align: center;
-  width: 100%; /* Utiliser toute la largeur disponible */
-  max-width: 500px; /* Limiter la largeur maximale pour ne pas être trop large sur les grands écrans */
+  width: 100%;
+  max-width: 500px;
   background: #fff;
   padding: 20px;
   border-radius: 10px;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  margin: 0 auto; /* Centrer horizontalement */
-  box-sizing: border-box; /* Assurer que padding ne dépasse pas de la taille spécifiée */
+  margin: 0 auto;
+  box-sizing: border-box;
 }
 
 .link-to-add {
@@ -161,52 +155,48 @@ onMounted(() => {
   justify-content: center;
 }
 
-/* Conteneur pour les catégories */
 .categories-container {
   display: flex;
   flex-wrap: wrap;
-  gap: 10px;
-  justify-content: center; /* Centrer les catégories horizontalement */
-  margin: 20px auto;
+  gap: 4px;
+  justify-content: center;
+  margin: 4px auto;
   width: 100%;
-  max-width: 700px; /* Limiter la largeur du conteneur des catégories */
+  max-width: 700px;
   box-sizing: border-box;
 }
 
-/* Style de chaque catégorie */
 .category {
   background-color: #f1f1f1;
   border: 2px solid #ccc;
-  padding: 8px 16px;
-  border-radius: 4px;
-  font-size: 16px;
-  font-weight: 600;
+  padding: 2px 4px; /* Réduction des marges internes */
+  border-radius: 2px;
+  font-size: 16px; /* Taille de police réduite */
+  font-weight: 700; /* Réduction de l'épaisseur de la police */
   text-align: center;
   cursor: pointer;
   transition: all 0.3s ease;
-  flex: 1 0 30%; /* Chaque catégorie prendra environ 30% de la largeur du conteneur */
+  flex: 1 0 18%; /* Taille plus petite pour afficher plus de catégories */
   box-sizing: border-box;
 }
 
-/* Effet au survol */
 .category:hover {
   border: 2px solid #007BFF;
   color: black;
   transform: scale(1.05);
 }
 
-/* Effet lors de la sélection */
 .category.selected {
   background-color: #ff1493;
   color: white;
   border-color: #ff1493;
 }
 
-/* Centrer le texte de chaque catégorie */
 .category span {
   display: block;
   text-align: center;
 }
+
 .show-index {
   margin-left: 8px;
 }
@@ -215,20 +205,18 @@ onMounted(() => {
   margin-left: 8px;
 }
 
-/* Responsive Design pour mobile */
 @media (max-width: 600px) {
   .container {
-    width: 90%; /* Plus large sur les petits écrans */
+    width: 90%;
   }
   .category {
-    flex: 1 0 45%; /* Sur les petits écrans, chaque catégorie prendra 45% de la largeur */
+    flex: 1 0 30%; /* Ajustement pour petits écrans */
   }
 }
 
 @media (max-width: 400px) {
   .category {
-    flex: 1 0 100%; /* Sur les écrans très petits, chaque catégorie prendra toute la largeur */
+    flex: 1 0 100%;
   }
 }
-
 </style>
